7df5936ab1193177e37e6610f63b62308495712d
martinezgomez34 <martinezgomez.edu34@gmail.com>
2024-03-21 09:48:13 -0600

    ProyectoJoyeria

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.eduard034.joyeria_proyectomult {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.eduard034.joyeria_proyectomult to javafx.fxml;
+    exports com.eduard034.joyeria_proyectomult;
+}
